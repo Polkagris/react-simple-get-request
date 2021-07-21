b8c5a80c59befc636e1779d07d06cc43c173b8ab
unknown <arne.n.bjerkaas@gmail.com>
2021-07-21 21:54:05 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      setFetchedData(data);
+    };
+    getData();
+  }, []);
+
+  console.log("data: ", fetchedData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>test</h1>
+      {fetchedData.data ? <h2>title: {fetchedData.data.title}</h2> : null}
     </div>
   );
 }
